c2fd9e6556bee6519ea8b8d8f045bdad06da21ba
Maxim Vasilyev <mayxis@inbox.ru>
2026-04-06 11:37:47 +0300

    feat: expose public API in src module

--- saga_orchestrator/__init__.py
+++ saga_orchestrator/__init__.py
@@ -1,1 +1,57 @@
-"""Source module."""
+"""Public package API for the saga orchestrator library."""
+
+from .admin import SagaAdmin
+from .core import SagaBuilder, SagaEngine, SagaOrchestrator, SagaRepository
+from .domain.exceptions import (
+    ActiveSagaAlreadyExistsError,
+    SagaDefinitionError,
+    SagaNotFoundError,
+    SagaStateError,
+    TypeValidationError,
+)
+from .domain.mixins import SagaStateMixin
+from .domain.models import (
+    BaseStep,
+    ExponentialRetry,
+    FixedRetry,
+    InputContext,
+    NoRetry,
+    RetryPolicy,
+    SagaAdminSnapshot,
+    SagaDefinition,
+    SagaSnapshot,
+    StepDefinition,
+    StepInputMap,
+    StepRef,
+)
+from .domain.models.enums import SagaStatus
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "ActiveSagaAlreadyExistsError",
+    "BaseStep",
+    "ExponentialRetry",
+    "FixedRetry",
+    "InputContext",
+    "NoRetry",
+    "RetryPolicy",
+    "SagaAdmin",
+    "SagaAdminSnapshot",
+    "SagaBuilder",
+    "SagaDefinition",
+    "SagaDefinitionError",
+    "SagaEngine",
+    "SagaNotFoundError",
+    "SagaOrchestrator",
+    "SagaRepository",
+    "SagaSnapshot",
+    "SagaStateError",
+    "SagaStateMixin",
+    "SagaStatus",
+    "StepDefinition",
+    "StepInputMap",
+    "StepRef",
+    "TypeValidationError",
+    "__version__",
+]
